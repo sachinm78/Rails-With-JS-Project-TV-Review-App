9b57d4fb128d44919f326c6e5c88915322cc4056
Sachinm78 <sachinm78@gmail.com>
2019-06-05 11:56:53 -0700

    reconfigured routes to make reviews a nested resource under users.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
   resources :shows
-  resources :reviews
+  resources :users do
+    resources :reviews
+  end
 end
