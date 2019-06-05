cbd2c7fe1fc384b88b56f88d23953f4b70e7f14f
Sachinm78 <sachinm78@gmail.com>
2019-06-05 16:37:48 -0700

    created CRUD methods in reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,42 @@
 class ReviewsController < ApplicationController::Base
 
     def index
+        @reviews = Review.all
     end
 
     def new
+        @review = review.new
+    end
+
+    def create
+        @review = Review.create(review_params)
+        if @review
+            redirect_to user_review_path(@review)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @review = Review.find_by(id: params[:id])
     end
 
     def edit
     end
 
-    def show 
+    def update
+        @review.update(review_params)
+        if @review.save
+            redirect_to user_review_path(@review)
+        else
+            render :edit
+        end
     end
 
-    def delete
+private
+    
+    def review_params
+        params.require(:review).permit(:rating, :comment)
     end
 
 end
