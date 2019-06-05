0aae01ed8b06e06ec7cf363a7637e3a12c33afd5
Sachinm78 <sachinm78@gmail.com>
2019-06-05 15:37:10 -0700

    successfully tested db tables in console, stubbed out routes in shows and reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController::Base
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show 
+    end
+
+    def delete
+    end
+
+end
---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,18 @@
+class ShowsController < ApplicationController::Base
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show 
+    end
+
+    def delete
+    end
+
+end
