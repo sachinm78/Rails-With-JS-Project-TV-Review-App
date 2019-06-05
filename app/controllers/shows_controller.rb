82c40ebce801b75c841dfcb4c0b7ea5755142bdf
Sachinm78 <sachinm78@gmail.com>
2019-06-05 16:32:21 -0700

    created CRUD methods in shows_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,18 +1,42 @@
 class ShowsController < ApplicationController::Base
 
     def index
+        @shows = Show.all
     end
 
     def new
+        @show = show.new
+    end
+
+    def create
+        @show = Show.create(show_params)
+        if @show
+            redirect_to show_path(@show)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @show = Show.find_by(id: params[:id])
     end
 
     def edit
     end
 
-    def show 
+    def update
+        @show.update(show_params)
+        if @show.save
+            redirect_to show_path(@show)
+        else
+            render :edit
+        end
     end
 
-    def delete
+private
+    
+    def show_params
+        params.require(:show).permit(:title, :genre)
     end
-
+    
 end
