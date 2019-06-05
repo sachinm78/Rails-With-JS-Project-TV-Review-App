9e66ce2808892cd456a5ef806cb1f29b59d46548
Sachinm78 <sachinm78@gmail.com>
2019-06-05 14:37:40 -0700

    corrected nested routes so shows are nested under users

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#welcome'
-  resources :shows
+  resources :reviews
   resources :users do
-    resources :reviews
+    resources :shows
   end
 end
