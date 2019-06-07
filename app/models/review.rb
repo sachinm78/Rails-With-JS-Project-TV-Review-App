5911f5cc68ea78b1bdbbcdc2170d6d1095e7f268
Sachinm78 <sachinm78@gmail.com>
2019-06-07 15:39:48 -0700

    added some scope methods to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,17 @@ class Review < ApplicationRecord
   belongs_to :show
 
   validates :rating, :numericality => { :less_than_or_equal_to => 5 }
+
+  def self.five_stars
+    all.where("rating = ?", 5)
+  end
+
+  def self.four_stars
+    all.where("rating = ?", 4)
+  end
+
+  def self.not_rated
+    all.where("rating = ?", nil)
+  end
+
 end
