e9d23c5ee520dedf9b8ff836dccd7e1421d77511
Sachinm78 <sachinm78@gmail.com>
2019-07-11 09:00:28 -0700

    set up home page and application controller to render json data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,37 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     
     def welcome
-        @user = current_user
-        @shows = Show.all
-        @reviews = Review.all
+        get_data
         respond_to do |f|
             f.html {render :welcome}
             f.json {render json: @shows, each_serializer: ShowSerializer, root: 'application#welcome'}
         end
+    end
+
+    def show 
+        get_data
+        find_review
+        respond_to do |f|
+            f.html {render :welcome}
+            f.json {render json: @review, each_serializer: ReviewSerializer, root: 'application#welcome'}
+        end
+    end
+
+    def next
+        get_data
+        find_review
+    end
+
+    private
+
+    def get_data
+        @user = current_user
+        @shows = Show.all
+        @reviews = Review.all
+    end
+
+    def find_review
+        @review = Review.find_by(id: params[:id])
     end
 
 end
